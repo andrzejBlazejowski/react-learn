c98b030253881038bb82d793502273918d0fbca7
Andrzej Blazejowski <andrzej.blazejowski@gmail.com>
2022-01-27 16:40:42 +0100

    added some state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Person from "./Person/Person";
 
 function App() {
+  const [persons, setPersons] = useState([
+    { name: "ann", age: "23", children: "useState" },
+    { name: "Dominik", age: "22", children: "useState" },
+  ]);
+  const switchNames = () => {
+    const copPersons = [...persons];
+    copPersons[0].name = persons[1].name;
+    copPersons[1].name = persons[0].name;
+    console.log(copPersons);
+    setPersons(copPersons);
+  };
   return (
     <div className="App">
-      <Person name="ann" age="23">
-        My Hobbies: Drawing
-      </Person>
+      <button onClick={switchNames}>switch names</button>
+      {persons.map((person, index) => {
+        return (
+          <Person key={index} name={person.name} age={person.age}>
+            {person.children}
+          </Person>
+        );
+      })}
     </div>
   );
 }
